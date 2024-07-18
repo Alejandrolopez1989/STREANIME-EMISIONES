25f5a7be5e59325884fff65460598343e87de36c
Alejandrolopez1989 <140138063+Alejandrolopez1989@users.noreply.github.com>
2024-07-18 00:29:30 -0400

    Update script login.js

--- script login.js
+++ script login.js
@@ -1,29 +1,25 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita el envío del formulario
-  
-    // Obtén el nombre de usuario y la contraseña ingresados
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-  
-    // Define un objeto o un array con los usuarios y contraseñas permitidos
-    var users = [
-      { username: "Keonex", password: "alejandro89" },   
-      { username: "Carlos Santana", password: "Guau0215" },
-      { username: "OskrBri", password: "Brihanna21" },
-      { username: "Karel", password: "Potato59" },
-      { username: "Alex dulac", password: "1234" },      
-      { username: "Jinx", password: "jinxjx" }
-    ];
-  
-    // Verifica si el usuario y la contraseña coinciden con los datos almacenados
-    for (var i = 0; i < users.length; i++) {
-      if (users[i].username === username && users[i].password === password) {
-        // Redirecciona a la página deseada si se encuentra una coincidencia
-        window.location.href = "inicio.html";
-        return;
-      }
+  event.preventDefault(); // Evita el envío del formulario
+
+  // Obtén el nombre de usuario y la contraseña ingresados
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+
+  // Define un objeto o un array con los usuarios y contraseñas permitidos
+  var users = [
+    { username: "Keonex", password: "alejandro89" },         
+    { username: "SKAFJAKSFJAKFJ", password: "SADKALFKAJFAKFJ" }
+  ];
+
+  // Verifica si el usuario y la contraseña coinciden con los datos almacenados
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].username === username && users[i].password === password) {
+      // Redirecciona a la página deseada si se encuentra una coincidencia
+      window.location.href = "inicio.html";
+      return;
     }
-  
-    // Si no se encuentra una coincidencia, muestra un mensaje de error
-    alert("Nombre de usuario o contraseña inválidos. Por favor, inténtalo de nuevo.");
-  });
+  }
+
+  // Si no se encuentra una coincidencia, muestra un mensaje de error
+  alert("Nombre de usuario o contraseña inválidos. Por favor, inténtalo de nuevo.");
+});
